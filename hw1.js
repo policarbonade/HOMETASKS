35ee359ad2b786dccaa2a359db87ebc9670307b0
Polly <n-armstrong@mail.ru>
2022-06-17 16:22:02 +0300

    UPDATED JS HOMETASK

--- hw1.js
+++ hw1.js
@@ -15,16 +15,35 @@ console.log('item_6 == ', typeof item_6, 'item_6_type', typeof item_6_type);
 let item_7 = String(item_6);
 let item_7_type = typeof type_7;
 console.log('item_7 == ', typeof item_7, 'item_7_type', typeof item_7_type);
-let age_1 = 10, age_2 = 18, age_3 = 60;
+
+
+function checkAge(age_1, age_2, age_3) {
+age_1 = prompt("Введите первый возраст", '');
+age_2 = prompt("Введите второй возраст", '');
+age_3 = prompt("И последний", '');
+
+if (!Number(age_1 || age_2 || age_3)) {
+    console.log("Error");
+}
+
+else if (Number(age_1 && age_2 && age_3)) {
 if (age_1 < age_2) {
-console.log("You don’t have access cause your age is", age_1,"It’s less then", age_2)
+console.log("You don’t have access cause your age is", age_1,"It’s less then", age_2);
 }
+
 else if (age_1 >= age_2 && age_1 < age_3) {
-console.log("Welcome!")
+console.log("Welcome!");
 }
+
 else if (age_1 > age_3) {
-console.log("Keep calm and watch Culture channel")
+console.log("Keep calm and watch Culture channel");
 }
+
 else {
-    console.log("Technical work")
+    console.log("Technical work");
 }
+} 
+}
+
+
+checkAge();
